a2ddeae8b0d633bb43da6aace0fe98648320578b
Jerron <97896336+iloveeuu@users.noreply.github.com>
2022-04-26 16:47:08 +0800

    Moved connection to DataContext

--- LepakRestaurant/Entity/DataContext.cs
+++ LepakRestaurant/Entity/DataContext.cs
@@ -7,6 +7,6 @@ namespace LepakRestaurant.Entity
 {
     public class DataContext
     {
-        public string connString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        public string connString = "workstation id=lepakrestaurant.mssql.somee.com;packet size = 4096; user id = csit314_SQLLogin_1; pwd=nw3fbejgvo;data source = lepakrestaurant.mssql.somee.com; persist security info=False;initial catalog = lepakrestaurant";
     }
 }
